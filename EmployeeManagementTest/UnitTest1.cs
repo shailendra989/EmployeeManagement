787506dc14e4897bd4fd77a0c910bef626adf80f
Shailendra kumar pal <palshailendrakumar123@gmail.com>
2023-02-20 22:06:25 +0530

    lo

---
+++ EmployeeManagementTest/UnitTest1.cs
@@ -0,0 +1,29 @@
+using EmployeeManagement;
+using EmployeeManagement.Model.SalaryModel;
+
+namespace EmployeeManagementTest
+{
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GivenSalaryDataAbleToUpdateSalaryDetails()
+        {
+            Salary salary = new Salary();
+            SalaryUpdateModel updateModel = new SalaryUpdateModel()
+            {
+                salaryId = 1,
+                Month = "Jan",
+                EmployeeSalary = 1300,
+                EmployeeId = 2
+            };
+
+            int EmpSalary = salary.UpdateEmployeeSalary(updateModel);
+
+            Assert.AreEqual(updateModel.EmployeeSalary, EmpSalary);
+        }
+    }
+
+
+}
